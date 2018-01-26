a4d301206a454d2b4cde2ccc6e9c6e3a4285eceb
Elitsohwws <novikovartem.khai@gmail.com>
2018-01-26 13:32:11 +0200

    Update test.php

--- test.php
+++ test.php
@@ -23,8 +23,8 @@
       margin-bottom: 0;
     }
     img {
-      height: 25%;
-      width: 50%;
+      height: 35%;
+      width: 35%;
     }
   </style>
 </head>
@@ -33,7 +33,7 @@
     <img src="logo.png"/>
     <h1>Congratulations!</h1>
     <h2>This application was AUTOMATICALLY deployed using AWS CodeDeploy from GitHub.</h2>
-    <h1>Version 47.0</h1>
+    <h1>Version 1.0</h1>
     <?php
       $id = file_get_contents("http://instance-data/latest/meta-data/instance-id");
       $ip = file_get_contents("http://instance-data/latest/meta-data/local-ipv4");
